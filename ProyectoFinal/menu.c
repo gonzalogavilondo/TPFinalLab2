3033e4a9b5b2d8cee6d257eb9be4b0babfd373ff
DESKTOP-NHC40B6\Gonzalo <gonzalogavilondo@gmail.com>
2023-11-19 20:59:25 -0300

    Se cambia color de menu.

--- ProyectoFinal/menu.c
+++ ProyectoFinal/menu.c
@@ -708,30 +708,38 @@ void cabeza(char a[]){
     imprimirCabecera(a);
 }
 
-void Rectangulo(){
-    system("color F1");
-    int lado=15;
-    int base=73;
-    int i=0;
-    while(i<base){
-        printf("%c",220);
-        i++;
-    }
-    i=0;
-    while(i<lado){
-        printf("\n%c\t\t\t\t\t\t\t\t\t%c",219,219);
-        i++;
-    }
-    i=0;
-    printf("\n");
-    printf("%c",219);
-    while(i<base-2){
-        printf("%c",220);
-        i++;
-    }
-    printf("%c",219);
+void Rectangulo() {
+    // Establecer el color del fondo a negro y el texto a amarillo
+    color(0); // Fondo negro
+    color(7); // Texto amarillo
 
+    int lado = 15;
+    int base = 73;
+    int i = 0;
+
+    while (i < base) {
+        printf("%c", 220);
+        i++;
+    }
+    i = 0;
+
+    while (i < lado) {
+        printf("\n%c\t\t\t\t\t\t\t\t\t%c", 219, 219);
+        i++;
+    }
+
+    i = 0;
+    printf("\n");
+    printf("%c", 219);
+
+    while (i < base - 2) {
+        printf("%c", 220);
+        i++;
+    }
+
+    printf("%c", 219);
 }
+
 
 void submenuManejoEmpleados() {
     char control = 's';;
